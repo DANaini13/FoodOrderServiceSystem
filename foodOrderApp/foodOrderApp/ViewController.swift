efdf4ad671204554e79ccc92ee7c92755f14aab1
Zeyong shan <shan27@pdx.edu>
2018-03-22 02:44:00 -0700

    init project

---
+++ foodOrderApp/foodOrderApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foodOrderApp
+//
+//  Created by NA-Sean on 3/22/18.
+//  Copyright © 2018 NAsoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
