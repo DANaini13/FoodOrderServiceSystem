846aecc4eee32784556520c48d5a229e5d4cc3a2
Zeyong Shan <shan27@pdx.edu>
2018-03-28 02:58:22 -0700

    添加了登陆和注册页面

---
+++ foodOrderApp/foodOrderApp/Account/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  foodOrderApp
+//
+//  Created by NA-Sean on 3/22/18.
+//  Copyright © 2018 NAsoftware. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak private var accountPlaceHolder: UITextField!
+    @IBOutlet weak private var passwordPlaceHolder: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction private func touchedLogin(_ sender: UIButton) {
+        guard accountPlaceHolder.text != nil && passwordPlaceHolder.text != nil  else {
+            return
+        }
+        let account = accountPlaceHolder.text!
+        let password = passwordPlaceHolder.text!
+        print(account)
+        print(password)
+    }
+    
+
+    
+}
+
---
+++ foodOrderApp/foodOrderApp/Account/RegisterViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterViewController.swift
+//  foodOrderApp
+//
+//  Created by sean on 3/28/18.
+//  Copyright © 2018 NAsoftware. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak private var accountPlaceHolder: UITextField!
+    @IBOutlet weak private var passwordPlaceHolder: UITextField!
+    @IBOutlet weak private var confrimPlaceHolder: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction private func touchedRegister(_ sender: UIButton) {
+        guard accountPlaceHolder.text != nil else {
+            return
+        }
+        guard passwordPlaceHolder.text == confrimPlaceHolder.text && confrimPlaceHolder.text != nil else {
+            return
+        }
+    }
+    
+}
