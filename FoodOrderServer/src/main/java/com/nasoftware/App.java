69836d9ca4379ee86bc1cb1ce7e40b55344841a5
sean <sean@naplingsdeMacBook-Air.local>
2018-04-03 16:16:42 -0700

    sql环境配置失败

--- FoodOrderServer/src/main/java/com/nasoftware/App.java
+++ FoodOrderServer/src/main/java/com/nasoftware/App.java
@@ -1,8 +1,10 @@
 package com.nasoftware;
 
+import java.sql.*;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Server start!");
         ServerManager serverManager = new ServerManager(8888);
         serverManager.start();
